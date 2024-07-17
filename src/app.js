f3bccc16fd12a3f73a3fac554821868d71960230
vishal <vishalbabar7351@gmail.com>
2024-07-17 20:07:38 +0530

    add user model and video model

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({limit: "16kb"})) //This middleware parses incoming JSON payloads and makes them available under req.body.
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(CookieParser())
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-// import express from "express";
 import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/db.conection.js";
 
 // Load environment variables from the .env file
 dotenv.config();
-
-// Create an Express app
-//const app = express();
 
 // Connect to MongoDB
 connectDB()
@@ -20,15 +16,3 @@ connectDB()
 console.log('MongoDB conection fail', error)
 })
 
-// Define the port
-// const PORT = process.env.PORT || 5000;
-
-// Define a simple route
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// Start the server
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}`);
-// });
